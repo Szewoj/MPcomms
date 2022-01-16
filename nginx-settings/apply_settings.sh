804cd106209155f9bf54fcab929397843000a4a1
Wojciech Szerszen <wojciech_szerszen@op.pl>
2022-01-16 22:34:00 +0100

    [fix] bash related issues

--- nginx-settings/apply_settings.sh
+++ nginx-settings/apply_settings.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 sudo cp "$SCRIPT_DIR/nginx.conf" "/usr/local/nginx/conf/nginx.conf"
--- stop_nginx.sh
+++ stop_nginx.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 sudo /usr/local/nginx/sbin/nginx -s stop
